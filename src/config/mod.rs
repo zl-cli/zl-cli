63946d145f8def23c6897b4aa3d854339e189daa
Lird <839122419@qq.com>
2023-06-17 20:39:36 +0800

    fix multierr

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use std::io::Write;
 use std::path::Path;
 use toml::de::Error;
 
@@ -11,5 +12,18 @@ impl ZConfig{
         let content = read_to_string(file)?.as_str();
 
         toml::from_str(content)
+    }
+
+    fn write_to_file(config: &ZConfig, file: &Path) -> Result<(), String>{
+        let content = match toml::to_string(config) {
+            Ok(c) => c,
+            Err(e) => {return Err(e.to_string());}
+        };
+        let mut config_file = match std::fs::File::open(file) {
+            Ok(f) => f,
+            Err(e) => {return Err(e.to_string());}
+        };
+        config_file.write_all(content.as_bytes());
+        Ok(())
     }
 }
