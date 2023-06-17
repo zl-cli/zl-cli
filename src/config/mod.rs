a586145b9d5a773c699dad18a7e07eaf16be3115
ScSOfts <scsofts.sc@gmail.com>
2023-06-17 21:11:23 +0800

    Modify: Clean code

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,26 +1,27 @@
 use std::fs::read_to_string;
 use std::io::Write;
 use std::path::Path;
-use toml::de::Error;
-
-use crate::ZError::ZError;
+use crate::zerror::ZError;
 
 mod config;
 
 pub type ZConfig = config::ZConfig;
 
+#[allow(dead_code)]
 impl ZConfig{
     fn from_file(file: &Path) -> Result<ZConfig, ZError>{
         let content = read_to_string(file)?;
-
         let config = toml::from_str(content.as_str())?;
+
         Ok(config)
     }
 
     fn write_to_file(config: &ZConfig, file: &Path) -> Result<(), ZError>{
         let content = toml::to_string(config)?;
         let mut config_file = std::fs::File::open(file)?;
-        config_file.write_all(content.as_bytes());
+
+        config_file.write_all(content.as_bytes())?;
+
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod config;
-mod ZError;
+mod zerror;
 
 fn main() {
     println!("Hello, world!");
